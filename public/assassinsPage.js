14069b2ac77d62abac6effb181e5a6ddfe1335a2
Jake Lewis <jakeaustinlewis@gmail.com>
2018-06-27 23:57:50 -0500

    added newAssassins and newContract html files

--- public/assassinsPage.js
+++ public/assassinsPage.js
@@ -1,9 +1,23 @@
 document.addEventListener("DOMContentLoaded", function (event) {
     assassinsContainer = document.getElementById('assassins-container');
 
-    // function populateAssassins () {
-    //     let assassin = 
-    // }
+    function populateAssassins () {
+        let assassin = document.createElement("h2");
+        assassins.classList.add
+    }
 
     populateAssassins();
 });
+
+
+function createPalette() {
+    for (let i = 0 ; i < paletteColors.length; i++) {
+        let circle = document.createElement("div");
+        circle.classList.add('palette');
+        circle.style.backgroundColor = paletteColors[i];
+        if (paletteColors[i]== 'white') {
+            circle.style.border= "1px solid black";
+        }
+        colorPalette.append(circle);
+    }
+}
