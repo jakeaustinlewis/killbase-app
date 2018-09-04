0767279f095c104ddc884fbce8319a1958c03e0c
Jake Lewis <jakeaustinlewis@gmail.com>
2018-09-04 13:25:16 -0500

    moved migrations_and_seeds file

--- knexfile.js
+++ knexfile.js
@@ -24,18 +24,6 @@ module.exports = {
 
   production: {
     client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user:     'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
+    connection: `postgres://abiwzeoblheeuu:adcbf127bdd5672fa8cf705fe8debee71a19527671df05bdbbf591e1d37eecfc@ec2-50-17-194-129.compute-1.amazonaws.com:5432/d3jqv3n3fs03e8`
   }
-
 };
